cf47b9cf86f3729627d84fd3147cf3b15666342e
Metin Melikşah Dermencioğlu <metindermencioglu@outlook.com>
2025-09-27 12:28:30 +0300

    Görseller eklendi.

--- FlappyBird/FlappyBird/Form1.Designer.cs
+++ FlappyBird/FlappyBird/Form1.Designer.cs
@@ -28,22 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.FlappyBirdpictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Flying_timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Fall_down_timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.FlappyBirdpictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // FlappyBirdpictureBox1
+            // 
+            this.FlappyBirdpictureBox1.BackgroundImage = global::FlappyBird.Properties.Resources.Flappy_Bird_1;
+            this.FlappyBirdpictureBox1.Location = new System.Drawing.Point(120, 280);
+            this.FlappyBirdpictureBox1.Name = "FlappyBirdpictureBox1";
+            this.FlappyBirdpictureBox1.Size = new System.Drawing.Size(170, 120);
+            this.FlappyBirdpictureBox1.TabIndex = 0;
+            this.FlappyBirdpictureBox1.TabStop = false;
+            this.FlappyBirdpictureBox1.Click += new System.EventHandler(this.FlappyBirdpictureBox1_Click);
+            // 
+            // Flying_timer1
+            // 
+            this.Flying_timer1.Enabled = true;
+            this.Flying_timer1.Tick += new System.EventHandler(this.Flying_timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Chocolate;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(217)))), ((int)(((byte)(234)))));
             this.ClientSize = new System.Drawing.Size(999, 761);
+            this.Controls.Add(this.FlappyBirdpictureBox1);
             this.Name = "Form1";
             this.Text = "Flappy Bird";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FlappyBirdpictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox FlappyBirdpictureBox1;
+        private System.Windows.Forms.Timer Flying_timer1;
+        private System.Windows.Forms.Timer Fall_down_timer1;
     }
 }
 
--- FlappyBird/FlappyBird/Form1.cs
+++ FlappyBird/FlappyBird/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,12 @@ using System.Windows.Forms;
 
 namespace FlappyBird
 {
+
     public partial class Form1 : Form
     {
+
+        bool Fly_Boolean = false;
+        int Target_Y_Location = 800;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +25,25 @@ namespace FlappyBird
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void FlappyBirdpictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Flying_timer1_Tick(object sender, EventArgs e)
+        {
+            if (Fly_Boolean)
+            {
+                FlappyBirdpictureBox1.Image=Properties.Resources.Flappy_Bird_1;
+                Fly_Boolean = false;
+            }
+            else
+            {
+                FlappyBirdpictureBox1.Image = Properties.Resources.Flappy_Bird_2;
+                Fly_Boolean = true;
+            }
         }
     }
 }
